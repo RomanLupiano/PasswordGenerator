f6fba6d92a2b82db45d40f942a89f4de730b40e4
Marcos Lupiano <romanlupiano@gmail.com>
2022-04-01 11:57:10 -0300

    Added app generator and templates urls and view

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('about', views.about),
+    path('', views.index)
 ]
